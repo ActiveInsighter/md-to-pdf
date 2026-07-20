827ff7ecaf24a857cd26a793f25c05ffdcefbb2e
Li Bowei <149855691+ActiveInsighter@users.noreply.github.com>
2026-07-20 22:06:42 +0800

    fix(frontend): adopt React Router 7 provider API

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,11 @@
 import { RouterProvider } from 'react-router-dom'
-import { RouteLoading } from '@/components/layout/RouteLoading'
 import { AppProviders } from './providers'
 import { router } from './router'
 
 export default function App() {
   return (
     <AppProviders>
-      <RouterProvider router={router} fallbackElement={<RouteLoading />} />
+      <RouterProvider router={router} />
     </AppProviders>
   )
 }
